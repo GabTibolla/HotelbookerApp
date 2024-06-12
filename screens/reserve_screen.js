f8993e221e466bf4fe00e25c42fc1b9343ba8fb6
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-12 09:04:25 -0300

    criação de tela de reserva

---
+++ screens/reserve_screen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function ReserveScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Reserve!</Text>
+        </View>
+    );
+}
+
+export default ReserveScreen;
