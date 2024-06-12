ba6ef2dc575014c5ca8e16eb6f95fcd07ae1ce7b
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-12 09:04:16 -0300

    alteração de icones

--- components/menu_component.js
+++ components/menu_component.js
@@ -4,14 +4,36 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home_screen';
 import SettingsScreen from '../screens/settings_screen';
+import ReserveScreen from '../screens/reserve_screen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Settings" component={SettingsScreen} />
+            <Tab.Screen
+                name="Feed"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen name="Settings" component={SettingsScreen} options={{
+                tabBarLabel: 'Hotels',
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="bed" color={color} size={size} />
+                ),
+            }}/>
+            <Tab.Screen name="Reserve" component={ReserveScreen} options={{
+                tabBarLabel: 'Reservas',
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="calendar" color={color} size={size} />
+                ),
+            }}/>
         </Tab.Navigator>
     );
 }
