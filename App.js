dcf28b6d2629498f4c6d5be80274c989fb3fc514
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-12 13:33:08 -0300

    remocao de importacao

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MyTabs from './components/menu_component';
 
-
 export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );
 }
