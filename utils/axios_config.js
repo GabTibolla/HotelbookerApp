73f8f6ebdd835c989b255f320638d9677e1c9345
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-13 11:59:54 -0300

    alteração de link da api

--- utils/axios_config.js
+++ utils/axios_config.js
@@ -3,9 +3,9 @@ import { Platform } from 'react-native';
 
 const getBaseURL = () => {
     if (Platform.OS === 'ios') {
-        return 'http://localhost:3001';
+        return 'http://192.168.100.7:8080';
     } else if (Platform.OS === 'android') {
-        return 'http://172.30.132.204:3001';
+        return 'http://192.168.100.7:8080';
         // for android emulator 'http://10.0.2.2:3001'
     }
 };
