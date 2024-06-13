f8ad82c444c65eecbc0582fc0b0b54243b5b771d
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-13 12:01:37 -0300

    adicionando nova tela ao stack do hotel

--- screens/hotel_stack.js
+++ screens/hotel_stack.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HotelScreen from '../screens/hotel_screen';
-import DetailHotelScreen from './detail_hotel_screen';
+import QuartoScreen from './quartos_screen';
+import CreateReserveScreen from '../screens/create_reserve_screen';
 
 const Stack = createStackNavigator();
 
@@ -9,7 +10,8 @@ function HotelStack() {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Hotéis" component={HotelScreen} />
-            <Stack.Screen name="Quartos" component={DetailHotelScreen} />
+            <Stack.Screen name="Quartos" component={QuartoScreen} />
+            <Stack.Screen name="Fazer Reserva" component={CreateReserveScreen} />
         </Stack.Navigator>
     );
 }
