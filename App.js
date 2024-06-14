a8ed99007f4989d05c4e577c823c63dc21fd951f
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-14 12:38:25 -0300

    feat: Add TokenContext to Login

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/login_screen';
 import MyTabs from './components/menu_component';
+import {TokenProvider} from "./utils/token_context";
+
+const Stack = createStackNavigator();
+
+function AuthNavigator() {
+    return (
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Main"
+                component={MyTabs}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
+    );
+}
 
 export default function App() {
     return (
         <NavigationContainer>
-            <MyTabs />
+            <TokenProvider>
+                <AuthNavigator />
+            </TokenProvider>
         </NavigationContainer>
     );
 }
