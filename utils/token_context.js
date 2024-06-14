9ab3f389b3f96e2860865d52e1ae98cd87bc7260
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-14 13:48:01 -0300

    feat: add role users login

--- utils/token_context.js
+++ utils/token_context.js
@@ -4,15 +4,24 @@ const TokenContext = createContext();
 
 export const TokenProvider = ({ children }) => {
     const [token, setToken] = useState(null);
+    const [role, setRole] = useState(null);
 
     return (
-        <TokenContext.Provider value={{ token, setToken }}>
+        <TokenContext.Provider value={{ token, setToken, role, setRole }}>
             {children}
         </TokenContext.Provider>
     );
 };
 
 export const useToken = () => {
+    const context = useContext(TokenContext);
+    if (!context) {
+        throw new Error('useToken must be used within a TokenProvider');
+    }
+    return context;
+};
+
+export const useRole = () => {
     const context = useContext(TokenContext);
     if (!context) {
         throw new Error('useToken must be used within a TokenProvider');
