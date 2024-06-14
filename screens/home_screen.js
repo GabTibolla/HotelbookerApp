5daff579d6bde9ddcef533fb84d7710451389074
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-14 13:48:20 -0300

    feat: add button logout

--- screens/home_screen.js
+++ screens/home_screen.js
@@ -1,6 +1,33 @@
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
+    useEffect(() => {
+        const headerRight = () => (
+            <MaterialCommunityIcons
+                name="logout"
+                size={25}
+                onPress={handleLogout}
+                style={{ marginRight: 15 }}
+            />
+        );
+
+        navigation.setOptions({
+            headerRight,
+        });
+    }, [navigation]);
+
+    const handleLogout = async () => {
+        await AsyncStorage.removeItem('jwtToken');
+        navigation.reset({
+            index: 0,
+            routes: [{ name: 'Login' }],
+        });
+    };
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Home!</Text>
